ca9a7c312499060fc2dd1b774517e7aeb0402a4c
Mostafa Moradian <mostafamoradian0@gmail.com>
2022-10-20 22:18:22 +0200

    Add soft/limits and reading system rlimits
    Use Infof instead of Printf for logging
    Currently there is a warning for the hitting the soft limit, and
    new connections will be rejected after hitting the hard limit. The
    soft/hard limit will be configurable. If not set, system rlimit is
    used.

--- network/postgresql_server.go
+++ network/postgresql_server.go
@@ -1,6 +1,7 @@
 package network
 
 import (
+	"syscall"
 	"time"
 
 	"github.com/panjf2000/gnet/v2"
@@ -10,14 +11,41 @@ import (
 type PostgreSQLServer struct {
 	gnet.BuiltinEventEngine
 
-	Address string
-	engine  gnet.Engine
-	Options []gnet.Option
+	Address   string
+	engine    gnet.Engine
+	Options   []gnet.Option
+	SoftLimit int
+	HardLimit int
+}
+
+func GetRLimit() syscall.Rlimit {
+	var limits syscall.Rlimit
+	if err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &limits); err != nil {
+		logrus.Error(err)
+	}
+	logrus.Infof("Current system soft limit: %d", limits.Cur)
+	logrus.Infof("Current system hard limit: %d", limits.Max)
+	return limits
 }
 
 func (p *PostgreSQLServer) OnBoot(engine gnet.Engine) gnet.Action {
 	p.engine = engine
-	logrus.Printf("PostgreSQL server is listening on %s\n", p.Address)
+
+	// Get the current limits
+	limits := GetRLimit()
+
+	// Set the soft and hard limits if they are not set
+	if p.SoftLimit == 0 {
+		p.SoftLimit = int(limits.Cur)
+		logrus.Info("Soft limit is not set, using the current system soft limit")
+	}
+
+	if p.HardLimit == 0 {
+		p.HardLimit = int(limits.Max)
+		logrus.Info("Hard limit is not set, using the current system hard limit")
+	}
+
+	logrus.Infof("PostgreSQL server is listening on %s\n", p.Address)
 	return gnet.None
 }
 
@@ -35,18 +63,27 @@ func (p *PostgreSQLServer) OnShutdown(engine gnet.Engine) {
 }
 
 func (p *PostgreSQLServer) OnOpen(c gnet.Conn) (out []byte, action gnet.Action) {
-	logrus.Printf("PostgreSQL server is opening a connection from %s", c.RemoteAddr().String())
+	if p.engine.CountConnections() >= p.SoftLimit {
+		logrus.Warn("Soft limit reached")
+	}
+	if p.engine.CountConnections() >= p.HardLimit {
+		logrus.Error("Hard limit reached")
+		c.Write([]byte("Hard limit reached\n"))
+		c.Close()
+		return nil, gnet.Close
+	}
+	logrus.Infof("PostgreSQL server is opening a connection from %s", c.RemoteAddr().String())
 	return []byte("connected\n"), gnet.None
 }
 
 func (p *PostgreSQLServer) OnClose(c gnet.Conn, err error) (action gnet.Action) {
-	logrus.Printf("PostgreSQL server is closing a connection from %s", c.RemoteAddr().String())
+	logrus.Infof("PostgreSQL server is closing a connection from %s", c.RemoteAddr().String())
 	return gnet.None
 }
 
 func (p *PostgreSQLServer) OnTick() (delay time.Duration, action gnet.Action) {
 	logrus.Println("PostgreSQL server is ticking...")
-	logrus.Printf("Active connections: %d", p.engine.CountConnections())
+	logrus.Infof("Active connections: %d", p.engine.CountConnections())
 	return time.Second * 5, gnet.None
 }
 
