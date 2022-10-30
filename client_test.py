43aac132cbe9b7959bae27904f0c71de5d0eb180
Mostafa Moradian <mostafamoradian0@gmail.com>
2022-10-30 20:55:07 +0100

    Fix python test script

--- client_test.py
+++ client_test.py
@@ -4,21 +4,52 @@ from concurrent.futures import ThreadPoolExecutor
 import psycopg
 
 
-def worker(id):
+def create_table():
     conn = None
     try:
         conn = psycopg.connect(host="localhost", port=15432, dbname="test",
                                user="postgres", password="postgres", sslmode="disable")
 
-        conn.execute("BEGIN TRANSACTION ISOLATION LEVEL SERIALIZABLE")
         conn.execute(
             "CREATE TABLE IF NOT EXISTS test (id serial PRIMARY KEY, num integer, data varchar);")
+        conn.close()
+    except KeyboardInterrupt:
+        if conn:
+            conn.close()
+        os._exit(0)
+    except Exception as e:
+        print("Worker %s: %s" % (id, e))
+
+    return
+
+
+def writer(id):
+    conn = None
+    try:
+        conn = psycopg.connect(host="localhost", port=15432, dbname="test",
+                               user="postgres", password="postgres", sslmode="disable")
+
         conn.execute("INSERT INTO test (num, data) VALUES (%s, %s)", (id, "abc'def"))
-        conn.execute("COMMIT")
+
+        conn.close()
+    except KeyboardInterrupt:
+        if conn:
+            conn.close()
+        os._exit(0)
+    except Exception as e:
+        print("Worker %s: %s" % (id, e))
+
+    return
+
+
+def reader():
+    conn = None
+    try:
+        conn = psycopg.connect(host="localhost", port=15432, dbname="test",
+                               user="postgres", password="postgres", sslmode="disable")
 
         for row in conn.execute("SELECT * FROM test;"):
             print("ID=%s, NUM=%s, DATA=%s" % row)
-
         # conn.execute("DROP TABLE test;")
         conn.close()
     except KeyboardInterrupt:
@@ -35,5 +66,11 @@ if __name__ == '__main__':
     with ThreadPoolExecutor(max_workers=10) as executor:
         # Create 11 connections to the server and run queries in parallel
         # This will cause the server to crash
-        for i in range(11):
-            executor.submit(worker, i)
+        executor.submit(create_table)
+
+        for i in range(10):
+            executor.submit(writer, i)
+
+        # Wait for all threads to finish
+        executor.submit(reader)
+        executor.shutdown(wait=True)
