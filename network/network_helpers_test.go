b7662da20fb6cfb17b1d0924a0ec27622a8c19aa
Mostafa Moradian <mostafamoradian0@gmail.com>
2022-11-01 19:07:24 +0100

    Initialize slice with 0 length

--- network/network_helpers_test.go
+++ network/network_helpers_test.go
@@ -3,7 +3,7 @@ package network
 import "encoding/binary"
 
 func CreatePostgreSQLPacket(typ byte, msg []byte) []byte {
-	packet := make([]byte, 1+4+len(msg))
+	packet := make([]byte, 0, 1+4+len(msg))
 
 	packet = append(packet, typ)
 	binary.BigEndian.PutUint32(packet, uint32(len(msg)+4))
