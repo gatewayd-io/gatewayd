e29aee00235bbf010c5b2192aa3bce71b34e998b
Mostafa Moradian <mostafamoradian0@gmail.com>
2022-11-03 19:54:08 +0100

    Add startup packet creation for testing

--- network/network_helpers_test.go
+++ network/network_helpers_test.go
@@ -1,6 +1,42 @@
 package network
 
-import "encoding/binary"
+import (
+	"encoding/binary"
+)
+
+type WriteBuffer struct {
+	Bytes []byte
+
+	msgStart   int
+	paramStart int
+}
+
+func writeStartupMsg(buf *WriteBuffer, user, database, appName string) {
+	// Write startup message header
+	buf.msgStart = len(buf.Bytes)
+	buf.Bytes = append(buf.Bytes, 0, 0, 0, 0)
+
+	// Write protocol version
+	buf.Bytes = append(buf.Bytes, 0, 0, 0, 0)
+	binary.BigEndian.PutUint32(buf.Bytes[len(buf.Bytes)-4:], uint32(196608))
+
+	buf.WriteString("user")
+	buf.WriteString(user)
+	buf.WriteString("database")
+	buf.WriteString(database)
+	buf.WriteString("application_name")
+	buf.WriteString(appName)
+	buf.WriteString("")
+
+	// Write message length
+	binary.BigEndian.PutUint32(
+		buf.Bytes[buf.msgStart:], uint32(len(buf.Bytes)-buf.msgStart))
+}
+
+func (buf *WriteBuffer) WriteString(s string) {
+	buf.Bytes = append(buf.Bytes, s...)
+	buf.Bytes = append(buf.Bytes, 0)
+}
 
 func CreatePostgreSQLPacket(typ byte, msg []byte) []byte {
 	packet := make([]byte, 1+4+len(msg))
@@ -12,4 +48,10 @@ func CreatePostgreSQLPacket(typ byte, msg []byte) []byte {
 	}
 
 	return packet
+}
+
+func CreatePgStartupPacket() []byte {
+	buf := &WriteBuffer{}
+	writeStartupMsg(buf, "postgres", "postgres", "gatewayd")
+	return buf.Bytes
 }
