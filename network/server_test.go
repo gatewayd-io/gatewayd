a981bc9faf23842dec361b736e9f8eb167a5a32a
Mostafa Moradian <mostafamoradian0@gmail.com>
2022-11-03 20:03:06 +0100

    Rewrite server tests to make it work in parallel

--- network/server_test.go
+++ network/server_test.go
@@ -1,10 +1,12 @@
 package network
 
 import (
+	"sync"
 	"testing"
 
 	embeddedpostgres "github.com/fergusstrange/embedded-postgres"
 	"github.com/panjf2000/gnet/v2"
+	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -14,46 +16,83 @@ func TestRunServer(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	server := &Server{
-		Network: "tcp",
-		Address: "localhost:15432",
-		Options: []gnet.Option{
-			gnet.WithMulticore(false),
-		},
-		SoftLimit: 1,
-		HardLimit: 2,
+	onIncomingTraffic := func(gconn gnet.Conn, cl *Client, buf []byte, err error) error {
+		logrus.Info("Incoming traffic")
+		assert.Equal(t, CreatePgStartupPacket(), buf)
+		assert.Nil(t, err)
+		return nil
 	}
+
+	onOutgoingTraffic := func(gconn gnet.Conn, cl *Client, buf []byte, err error) error {
+		logrus.Info("Outgoing traffic")
+		assert.Equal(t, CreatePostgreSQLPacket('R', []byte{0x0, 0x0, 0x0, 0x3}), buf)
+		assert.Nil(t, err)
+		return nil
+	}
+
+	// Create a server
+	server := NewServer(
+		"tcp",
+		"127.0.0.1:15432",
+		0,
+		0,
+		DefaultTickInterval,
+		2,
+		DefaultBufferSize,
+		false,
+		false,
+		[]gnet.Option{
+			gnet.WithMulticore(true),
+		},
+		onIncomingTraffic,
+		onOutgoingTraffic,
+	)
 	assert.NotNil(t, server)
+
+	var wg sync.WaitGroup
+	wg.Add(2)
 
 	go func(t *testing.T, server *Server) {
 		t.Helper()
+		defer wg.Done()
 
 		server.Run()
 	}(t, server)
 
-	for {
-		if server.Status == Running {
-			client := NewClient("tcp", "localhost:15432", DefaultBufferSize)
-			defer client.Close()
+	go func(t *testing.T, server *Server) {
+		t.Helper()
+		defer wg.Done()
 
-			assert.NotNil(t, client)
-			err := client.Send(CreatePostgreSQLPacket('Q', []byte("select 1;")))
-			assert.Nil(t, err)
+		for {
+			if server.IsRunning() {
+				client := NewClient("tcp", "127.0.0.1:15432", DefaultBufferSize)
+				defer client.Close()
 
-			size, data, err := client.Receive()
-			msg := "SFATAL\x00VFATAL\x00C0A000\x00Munsupported frontend protocol 0.0: " +
-				"server supports 3.0 to 3.0\x00Fpostmaster.c\x00L2138\x00R" + "ProcessStartupPacket\x00\x00"
-			assert.Equal(t, 132, size)
-			assert.Equal(t, len(data[:size]), size)
-			assert.Nil(t, err)
-			assert.NotEmpty(t, data[:size])
-			assert.Equal(t, msg, string(data[5:size]))
-			assert.Equal(t, "E", string(data[0]))
+				assert.NotNil(t, client)
+				err := client.Send(CreatePgStartupPacket())
+				assert.Nil(t, err)
 
-			// Clean up
-			server.Shutdown()
-			assert.NoError(t, postgres.Stop())
-			break
+				// The server should respond with a 'R' packet
+				size, data, err := client.Receive()
+				msg := []byte{0x0, 0x0, 0x0, 0x3}
+				// This includes the message type, length and the message itself
+				assert.Equal(t, 9, size)
+				assert.Equal(t, len(data[:size]), size)
+				assert.Nil(t, err)
+				packetSize := int(data[1])<<24 | int(data[2])<<16 | int(data[3])<<8 | int(data[4])
+				assert.Equal(t, 8, packetSize)
+				assert.NotEmpty(t, data[:size])
+				assert.Equal(t, msg, data[5:size])
+				// AuthenticationOk
+				assert.Equal(t, uint8(0x52), data[0])
+
+				// Clean up
+				server.Shutdown()
+				assert.NoError(t, postgres.Stop())
+				return
+			}
 		}
-	}
+	}(t, server)
+
+	wg.Wait()
 }
