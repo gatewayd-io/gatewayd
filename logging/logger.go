5efe90b2c371a3d0aeffc6e2f9dea5459f0c2a57
Mostafa Moradian <mostafamoradian0@gmail.com>
2022-11-04 20:49:39 +0100

    Replace logrus with zerolog

---
+++ logging/logger.go
@@ -0,0 +1,39 @@
+package logging
+
+import (
+	"io"
+	"os"
+
+	"github.com/rs/zerolog"
+)
+
+func NewLogger(
+	writer io.Writer,
+	timeFieldFormat string,
+	level zerolog.Level,
+	timestamp bool,
+) zerolog.Logger {
+	// Create a new logger
+	consoleWriter := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: timeFieldFormat}
+
+	if writer == nil {
+		// Default to stdout
+		writer = consoleWriter
+	}
+
+	if timeFieldFormat == "" {
+		timeFieldFormat = zerolog.TimeFieldFormat
+	}
+
+	zerolog.SetGlobalLevel(level)
+
+	// Create a new logger
+	logger := zerolog.New(writer)
+	if timestamp {
+		logger = logger.With().Timestamp().Logger()
+	}
+
+	logger.Debug().Msg("Created a new logger")
+
+	return logger
+}
