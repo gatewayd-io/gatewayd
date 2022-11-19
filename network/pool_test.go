a0171845d798e04aa542f339051cdc88f6b23fb0
Mostafa Moradian <mostafamoradian0@gmail.com>
2022-11-19 23:17:32 +0100

    Update tests to reflect changes to making the pool generic

--- network/pool_test.go
+++ network/pool_test.go
@@ -19,7 +19,23 @@ func TestNewPool(t *testing.T) {
 
 	logger := logging.NewLogger(cfg)
 	pool := NewPool(logger, 0, nil, nil)
-	defer pool.Close()
+	defer pool.Clear()
+	assert.NotNil(t, pool)
+	assert.NotNil(t, pool.Pool())
+	assert.Equal(t, 0, pool.Size())
+}
+
+func TestNewEmptyPool(t *testing.T) {
+	cfg := logging.LoggerConfig{
+		Output:     nil,
+		TimeFormat: zerolog.TimeFormatUnix,
+		Level:      zerolog.DebugLevel,
+		NoColor:    true,
+	}
+
+	logger := logging.NewLogger(cfg)
+	pool := NewEmptyPool(logger)
+	defer pool.Clear()
 	assert.NotNil(t, pool)
 	assert.NotNil(t, pool.Pool())
 	assert.Equal(t, 0, pool.Size())
@@ -47,13 +63,15 @@ func TestPool_Put(t *testing.T) {
 	logger := logging.NewLogger(cfg)
 
 	pool := NewPool(logger, 0, nil, nil)
-	defer pool.Close()
+	defer pool.Clear()
 	assert.NotNil(t, pool)
 	assert.NotNil(t, pool.Pool())
 	assert.Equal(t, 0, pool.Size())
-	assert.NoError(t, pool.Put(NewClient("tcp", "localhost:5432", DefaultBufferSize, logger)))
+	client1 := NewClient("tcp", "localhost:5432", DefaultBufferSize, logger)
+	pool.Put(client1.ID, client1)
 	assert.Equal(t, 1, pool.Size())
-	assert.NoError(t, pool.Put(NewClient("tcp", "localhost:5432", DefaultBufferSize, logger)))
+	client2 := NewClient("tcp", "localhost:5432", DefaultBufferSize, logger)
+	pool.Put(client2.ID, client2)
 	assert.Equal(t, 2, pool.Size())
 }
 
@@ -79,25 +97,25 @@ func TestPool_Pop(t *testing.T) {
 	logger := logging.NewLogger(cfg)
 
 	pool := NewPool(logger, 0, nil, nil)
-	defer pool.Close()
+	defer pool.Clear()
 	assert.NotNil(t, pool)
 	assert.NotNil(t, pool.Pool())
 	assert.Equal(t, 0, pool.Size())
 	client1 := NewClient("tcp", "localhost:5432", DefaultBufferSize, logger)
-	assert.NoError(t, pool.Put(client1))
+	pool.Put(client1.ID, client1)
 	assert.Equal(t, 1, pool.Size())
 	client2 := NewClient("tcp", "localhost:5432", DefaultBufferSize, logger)
-	assert.NoError(t, pool.Put(client2))
+	pool.Put(client2.ID, client2)
 	assert.Equal(t, 2, pool.Size())
-	client := pool.Pop(client1.ID)
+	client := pool.Pop(client1.ID).(*Client)
 	assert.Equal(t, client1.ID, client.ID)
 	assert.Equal(t, 1, pool.Size())
-	client = pool.Pop(client2.ID)
+	client = pool.Pop(client2.ID).(*Client)
 	assert.Equal(t, client2.ID, client.ID)
 	assert.Equal(t, 0, pool.Size())
 }
 
-func TestPool_Close(t *testing.T) {
+func TestPool_Clear(t *testing.T) {
 	postgres := embeddedpostgres.NewDatabase()
 	if err := postgres.Start(); err != nil {
 		t.Fatal(err)
@@ -119,53 +137,17 @@ func TestPool_Close(t *testing.T) {
 	logger := logging.NewLogger(cfg)
 
 	pool := NewPool(logger, 0, nil, nil)
+	defer pool.Clear()
 	assert.NotNil(t, pool)
 	assert.NotNil(t, pool.Pool())
 	assert.Equal(t, 0, pool.Size())
 	client1 := NewClient("tcp", "localhost:5432", DefaultBufferSize, logger)
-	assert.NoError(t, pool.Put(client1))
+	pool.Put(client1.ID, client1)
 	assert.Equal(t, 1, pool.Size())
 	client2 := NewClient("tcp", "localhost:5432", DefaultBufferSize, logger)
-	assert.NoError(t, pool.Put(client2))
+	pool.Put(client2.ID, client2)
 	assert.Equal(t, 2, pool.Size())
-	err := pool.Close()
-	assert.Nil(t, err)
-	assert.Equal(t, 2, pool.Size())
-}
-
-func TestPool_Shutdown(t *testing.T) {
-	postgres := embeddedpostgres.NewDatabase()
-	if err := postgres.Start(); err != nil {
-		t.Fatal(err)
-	}
-
-	defer func() {
-		if err := postgres.Stop(); err != nil {
-			t.Fatal(err)
-		}
-	}()
-
-	cfg := logging.LoggerConfig{
-		Output:     nil,
-		TimeFormat: zerolog.TimeFormatUnix,
-		Level:      zerolog.DebugLevel,
-		NoColor:    true,
-	}
-
-	logger := logging.NewLogger(cfg)
-
-	pool := NewPool(logger, 0, nil, nil)
-	defer pool.Close()
-	assert.NotNil(t, pool)
-	assert.NotNil(t, pool.Pool())
-	assert.Equal(t, 0, pool.Size())
-	client1 := NewClient("tcp", "localhost:5432", DefaultBufferSize, logger)
-	assert.NoError(t, pool.Put(client1))
-	assert.Equal(t, 1, pool.Size())
-	client2 := NewClient("tcp", "localhost:5432", DefaultBufferSize, logger)
-	assert.NoError(t, pool.Put(client2))
-	assert.Equal(t, 2, pool.Size())
-	pool.Shutdown()
+	pool.Clear()
 	assert.Equal(t, 0, pool.Size())
 }
 
@@ -191,23 +173,23 @@ func TestPool_ForEach(t *testing.T) {
 	logger := logging.NewLogger(cfg)
 
 	pool := NewPool(logger, 0, nil, nil)
-	defer pool.Close()
+	defer pool.Clear()
 	assert.NotNil(t, pool)
 	assert.NotNil(t, pool.Pool())
 	assert.Equal(t, 0, pool.Size())
 	client1 := NewClient("tcp", "localhost:5432", DefaultBufferSize, logger)
-	assert.NoError(t, pool.Put(client1))
+	pool.Put(client1.ID, client1)
 	assert.Equal(t, 1, pool.Size())
 	client2 := NewClient("tcp", "localhost:5432", DefaultBufferSize, logger)
-	assert.NoError(t, pool.Put(client2))
+	pool.Put(client2.ID, client2)
 	assert.Equal(t, 2, pool.Size())
-	pool.ForEach(func(client *Client) error {
-		assert.NotNil(t, client)
-		return nil
+	pool.ForEach(func(key, value interface{}) bool {
+		assert.NotNil(t, value.(*Client))
+		return true
 	})
 }
 
-func TestPool_ClientIDs(t *testing.T) {
+func TestPool_GetClientIDs(t *testing.T) {
 	postgres := embeddedpostgres.NewDatabase()
 	if err := postgres.Start(); err != nil {
 		t.Fatal(err)
@@ -229,16 +211,26 @@ func TestPool_ClientIDs(t *testing.T) {
 	logger := logging.NewLogger(cfg)
 
 	pool := NewPool(logger, 0, nil, nil)
-	defer pool.Close()
+	defer pool.Clear()
 	assert.NotNil(t, pool)
 	assert.NotNil(t, pool.Pool())
 	assert.Equal(t, 0, pool.Size())
 	client1 := NewClient("tcp", "localhost:5432", DefaultBufferSize, logger)
-	assert.NoError(t, pool.Put(client1))
+	pool.Put(client1.ID, client1)
 	assert.Equal(t, 1, pool.Size())
 	client2 := NewClient("tcp", "localhost:5432", DefaultBufferSize, logger)
-	assert.NoError(t, pool.Put(client2))
+	pool.Put(client2.ID, client2)
 	assert.Equal(t, 2, pool.Size())
-	ids := pool.ClientIDs()
+
+	var ids []string
+	pool.ForEach(func(key, value interface{}) bool {
+		ids = append(ids, key.(string))
+		return true
+	})
 	assert.Equal(t, 2, len(ids))
+	assert.Equal(t, client1.ID, ids[0])
+	assert.Equal(t, client2.ID, ids[1])
+	client1.Close()
+	client2.Close()
+	pool.Clear()
 }
