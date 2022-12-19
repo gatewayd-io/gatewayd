280e62a4e6097bbb07a8e8de345a31670905e44b
Mostafa Moradian <mostafamoradian0@gmail.com>
2022-12-19 19:51:26 +0100

    Add GatewayDError to handle errors and error wrapping
    Add error codes
    Add new errors

--- errors/errors.go
+++ errors/errors.go
@@ -1,22 +1,83 @@
 package errors
 
-import "errors"
+const (
+	ErrCodeUnknown ErrCode = iota
+	ErrCodeClientNotFound
+	ErrCodeClientNotConnected
+	ErrCodeClientConnectionFailed
+	ErrCodeNetworkNotSupported
+	ErrCodeResolveFailed
+	ErrCodePoolExhausted
+	ErrCodeStartServerFailed
+	ErrCodePluginNotFound
+	ErrCodePluginNotReady
+	ErrCodeStartPluginFailed
+	ErrCodeGetRPCClientFailed
+	ErrCodeDispensePluginFailed
+	ErrCodeClientReceiveFailed
+	ErrCodeClientSendFailed
+	ErrCodeServerReceiveFailed
+	ErrCodeServerSendFailed
+	ErrCodePutFailed
+	ErrCodeCastFailed
+	ErrCodeHookVerificationFailed
+	ErrCodeFileNotFound
+	ErrCodeFileOpenFailed
+	ErrCodeFileReadFailed
+)
 
 var (
-	ErrClientNotFound      = errors.New("client not found")
-	ErrNetworkNotSupported = errors.New("network is not supported")
-	ErrClientNotConnected  = errors.New("client is not connected")
-	ErrPoolExhausted       = errors.New("pool is exhausted")
+	ErrClientNotFound = NewGatewaydError(
+		ErrCodeClientNotFound, "client not found", nil)
+	ErrClientNotConnected = NewGatewaydError(
+		ErrCodeClientNotConnected, "client is not connected", nil)
+	ErrClientConnectionFailed = NewGatewaydError(
+		ErrCodeClientConnectionFailed, "failed to create a new connection", nil)
+	ErrNetworkNotSupported = NewGatewaydError(
+		ErrCodeNetworkNotSupported, "network is not supported", nil)
+	ErrResolveFailed = NewGatewaydError(
+		ErrCodeResolveFailed, "failed to resolve address", nil)
+	ErrPoolExhausted = NewGatewaydError(
+		ErrCodePoolExhausted, "pool is exhausted", nil)
+	ErrFailedToStartServer = NewGatewaydError(
+		ErrCodeStartServerFailed, "failed to start server", nil)
 
-	ErrPluginNotFound = errors.New("plugin not found")
-	ErrPluginNotReady = errors.New("plugin is not ready")
+	ErrPluginNotFound = NewGatewaydError(
+		ErrCodePluginNotFound, "plugin not found", nil)
+	ErrPluginNotReady = NewGatewaydError(
+		ErrCodePluginNotReady, "plugin is not ready", nil)
+	ErrFailedToStartPlugin = NewGatewaydError(
+		ErrCodeStartPluginFailed, "failed to start plugin", nil)
+	ErrFailedToGetRPCClient = NewGatewaydError(
+		ErrCodeGetRPCClientFailed, "failed to get RPC client", nil)
+	ErrFailedToDispensePlugin = NewGatewaydError(
+		ErrCodeDispensePluginFailed, "failed to dispense plugin", nil)
 
-	ErrClientReceiveFailed = errors.New("couldn't receive data from the server")
-	ErrClientSendFailed    = errors.New("couldn't send data to the server")
+	ErrClientReceiveFailed = NewGatewaydError(
+		ErrCodeClientReceiveFailed, "couldn't receive data from the server", nil)
+	ErrClientSendFailed = NewGatewaydError(
+		ErrCodeClientSendFailed, "couldn't send data to the server", nil)
 
-	ErrPutFailed = errors.New("failed to put in pool")
+	ErrServerSendFailed = NewGatewaydError(
+		ErrCodeServerSendFailed, "couldn't send data to the client", nil)
+	ErrServerReceiveFailed = NewGatewaydError(
+		ErrCodeServerReceiveFailed, "couldn't receive data from the client", nil)
 
-	ErrCastFailed = errors.New("failed to cast")
+	ErrPutFailed = NewGatewaydError(
+		ErrCodePutFailed, "failed to put in pool", nil)
+
+	ErrCastFailed = NewGatewaydError(
+		ErrCodeCastFailed, "failed to cast", nil)
+
+	ErrHookVerificationFailed = NewGatewaydError(
+		ErrCodeHookVerificationFailed, "failed to verify hook", nil)
+
+	ErrFileNotFound = NewGatewaydError(
+		ErrCodeFileNotFound, "file not found", nil)
+	ErrFileOpenFailed = NewGatewaydError(
+		ErrCodeFileOpenFailed, "failed to open file", nil)
+	ErrFileReadFailed = NewGatewaydError(
+		ErrCodeFileReadFailed, "failed to read file", nil)
 )
 
 const (
