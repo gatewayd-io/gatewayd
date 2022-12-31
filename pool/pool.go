0d34d19475a6e60bc236180ce104ad9990c25836
Mostafa Moradian <mostafamoradian0@gmail.com>
2022-12-31 14:49:26 +0100

    Add more comments for pool package

--- pool/pool.go
+++ pool/pool.go
@@ -32,14 +32,17 @@ type Impl struct {
 
 var _ Pool = &Impl{}
 
+// ForEach iterates over the pool and calls the callback function for each key/value pair.
 func (p *Impl) ForEach(cb Callback) {
 	p.pool.Range(cb)
 }
 
+// Pool returns the underlying sync.Map.
 func (p *Impl) Pool() *sync.Map {
 	return &p.pool
 }
 
+// Put adds a new key/value pair to the pool.
 func (p *Impl) Put(key, value interface{}) *gerr.GatewayDError {
 	if p.cap > 0 && p.Size() >= p.cap {
 		return gerr.ErrPoolExhausted
@@ -48,6 +51,7 @@ func (p *Impl) Put(key, value interface{}) *gerr.GatewayDError {
 	return nil
 }
 
+// Get returns the value for the given key.
 func (p *Impl) Get(key interface{}) interface{} {
 	if value, ok := p.pool.Load(key); ok {
 		return value
@@ -55,6 +59,8 @@ func (p *Impl) Get(key interface{}) interface{} {
 	return nil
 }
 
+// GetOrPut returns the value for the given key if it exists, otherwise it adds
+// the key/value pair to the pool.
 func (p *Impl) GetOrPut(key, value interface{}) (interface{}, bool, *gerr.GatewayDError) {
 	if p.cap > 0 && p.Size() >= p.cap {
 		return nil, false, gerr.ErrPoolExhausted
@@ -63,6 +69,7 @@ func (p *Impl) GetOrPut(key, value interface{}) (interface{}, bool, *gerr.Gatewa
 	return val, loaded, nil
 }
 
+// Pop removes the key/value pair from the pool and returns the value.
 func (p *Impl) Pop(key interface{}) interface{} {
 	if p.Size() == 0 {
 		return nil
@@ -73,6 +80,7 @@ func (p *Impl) Pop(key interface{}) interface{} {
 	return nil
 }
 
+// Remove removes the key/value pair from the pool.
 func (p *Impl) Remove(key interface{}) {
 	if p.Size() == 0 {
 		return
@@ -82,6 +90,7 @@ func (p *Impl) Remove(key interface{}) {
 	}
 }
 
+// Size returns the number of key/value pairs in the pool.
 func (p *Impl) Size() int {
 	var size int
 	p.pool.Range(func(_, _ interface{}) bool {
@@ -92,14 +101,18 @@ func (p *Impl) Size() int {
 	return size
 }
 
+// Clear removes all key/value pairs from the pool.
 func (p *Impl) Clear() {
 	p.pool = sync.Map{}
 }
 
+// Cap returns the capacity of the pool.
 func (p *Impl) Cap() int {
 	return p.cap
 }
 
+// NewPool creates a new pool with the given capacity.
+//
 //nolint:predeclared
 func NewPool(cap int) *Impl {
 	return &Impl{pool: sync.Map{}, cap: cap}
