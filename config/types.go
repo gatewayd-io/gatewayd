a7f2f91d2a776e2f1cf771af10d9edc975bcf12f
Mostafa Moradian <mostafamoradian0@gmail.com>
2023-01-06 02:44:08 +0100

    Move config_parser.go into config package and refactored it completely
    Add config structs to dump consolidated config into them for easier access
    Extract (almost) all the constants in all the packages into config/constants.go
    Add getter functions for some fields with validations

---
+++ config/types.go
@@ -0,0 +1,148 @@
+package config
+
+import (
+	"time"
+
+	"github.com/knadh/koanf"
+	"github.com/knadh/koanf/providers/confmap"
+)
+
+// // getPath returns the path to the referenced config value.
+// func getPath(cfg *koanf.Koanf, path string) string {
+// 	ref := cfg.String(path)
+// 	if cfg.Exists(path) && cfg.StringMap(ref) != nil {
+// 		return ref
+// 	}
+
+// 	return path
+// }
+
+type Plugin struct {
+	Name      string   `koanf:"name"`
+	Enabled   bool     `koanf:"enabled"`
+	LocalPath string   `koanf:"localPath"`
+	Args      []string `koanf:"args"`
+	Env       []string `koanf:"env"`
+	Checksum  string   `koanf:"checksum"`
+}
+
+type PluginConfig struct {
+	VerificationPolicy  string   `koanf:"verificationPolicy"`
+	CompatibilityPolicy string   `koanf:"compatibilityPolicy"`
+	Plugins             []Plugin `koanf:"plugins"`
+}
+
+type Client struct {
+	Network            string        `koanf:"network"`
+	Address            string        `koanf:"address"`
+	TCPKeepAlive       bool          `koanf:"tcpKeepAlive"`
+	TCPKeepAlivePeriod time.Duration `koanf:"tcpKeepAlivePeriod"`
+	ReceiveBufferSize  int           `koanf:"receiveBufferSize"`
+	ReceiveChunkSize   int           `koanf:"receiveChunkSize"`
+	ReceiveDeadline    time.Duration `koanf:"receiveDeadline"`
+	SendDeadline       time.Duration `koanf:"sendDeadline"`
+}
+
+type Logger struct {
+	Output     string `koanf:"output"`
+	FileName   string `koanf:"fileName"`
+	TimeFormat string `koanf:"timeFormat"`
+	Level      string `koanf:"level"`
+	NoColor    bool   `koanf:"noColor"`
+}
+
+type Pool struct {
+	Size int `koanf:"size"`
+}
+
+type Proxy struct {
+	Elastic             bool `koanf:"elastic"`
+	ReuseElasticClients bool `koanf:"reuseElasticClients"`
+}
+
+type Server struct {
+	Network          string        `koanf:"network"`
+	Address          string        `koanf:"address"`
+	SoftLimit        uint64        `koanf:"softLimit"`
+	HardLimit        uint64        `koanf:"hardLimit"`
+	EnableTicker     bool          `koanf:"enableTicker"`
+	MultiCore        bool          `koanf:"multiCore"`
+	LockOSThread     bool          `koanf:"lockOSThread"`
+	ReuseAddress     bool          `koanf:"reuseAddress"`
+	ReusePort        bool          `koanf:"reusePort"`
+	LoadBalancer     string        `koanf:"loadBalancer"`
+	TickInterval     time.Duration `koanf:"tickInterval"`
+	ReadBufferCap    int           `koanf:"readBufferCap"`
+	WriteBufferCap   int           `koanf:"writeBufferCap"`
+	SocketRecvBuffer int           `koanf:"socketRecvBuffer"`
+	SocketSendBuffer int           `koanf:"socketSendBuffer"`
+	TCPKeepAlive     time.Duration `koanf:"tcpKeepAlive"`
+	TCPNoDelay       bool          `koanf:"tcpNoDelay"`
+}
+
+type GlobalConfig struct {
+	Loggers map[string]Logger `koanf:"loggers"`
+	Clients map[string]Client `koanf:"clients"`
+	Pools   map[string]Pool   `koanf:"pools"`
+	Proxy   map[string]Proxy  `koanf:"proxy"`
+	Server  Server            `koanf:"server"`
+}
+
+// LoadDefaultConfig loads the default configuration before loading the config file.
+func LoadGlobalConfigDefaults(cfg *koanf.Koanf) {
+	defaultValues := confmap.Provider(map[string]interface{}{
+		"loggers": map[string]interface{}{
+			"default": map[string]interface{}{
+				"output": DefaultLogOutput,
+				"level":  "info",
+			},
+		},
+		"clients": map[string]interface{}{
+			"default": map[string]interface{}{
+				"receiveBufferSize":  DefaultBufferSize,
+				"receiveChunkSize":   DefaultChunkSize,
+				"tcpKeepAlivePeriod": DefaultTCPKeepAlivePeriod,
+			},
+		},
+		"pools": map[string]interface{}{
+			"default": map[string]interface{}{
+				"size": DefaultPoolSize,
+			},
+		},
+		"proxy": map[string]interface{}{
+			"default": map[string]interface{}{
+				"elastic":             false,
+				"reuseElasticClients": false,
+			},
+		},
+		"server": map[string]interface{}{
+			"network":          "tcp",
+			"address":          "0.0.0.0:15432",
+			"softLimit":        0,
+			"hardLimit":        0,
+			"enableTicker":     false,
+			"multiCore":        true,
+			"lockOSThread":     false,
+			"reuseAddress":     true,
+			"reusePort":        true,
+			"loadBalancer":     "roundrobin",
+			"readBufferCap":    DefaultBufferSize,
+			"writeBufferCap":   DefaultBufferSize,
+			"socketRecvBuffer": DefaultBufferSize,
+			"socketSendBuffer": DefaultBufferSize,
+		},
+	}, "")
+
+	cfg.Load(defaultValues, nil)
+}
+
+func LoadPluginConfigDefaults(cfg *koanf.Koanf) {
+	defaultValues := confmap.Provider(map[string]interface{}{
+		"plugins": map[string]interface{}{
+			"verificationPolicy":  "passdown",
+			"compatibilityPolicy": "strict",
+		},
+	}, "")
+
+	cfg.Load(defaultValues, nil)
+}
