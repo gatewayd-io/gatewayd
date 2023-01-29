b94c1ba46ad205ba7ca0aba563383ee8696c533a
Mostafa Moradian <mostafa@gatewayd.io>
2023-01-29 22:21:22 +0100

    Remove global variables

--- cmd/run.go
+++ cmd/run.go
@@ -24,11 +24,6 @@ import (
 )
 
 var (
-	globalConfigFile string
-	pluginConfigFile string
-)
-
-var (
 	hooksConfig   = hook.NewHookConfig()
 	DefaultLogger = logging.NewLogger(
 		logging.LoggerConfig{
@@ -364,12 +359,10 @@ var runCmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(runCmd)
 
-	runCmd.Flags().StringVarP(
-		&globalConfigFile,
+	runCmd.Flags().StringP(
 		"config", "c", "./gatewayd.yaml",
 		"config file (default is ./gatewayd.yaml)")
-	runCmd.Flags().StringVarP(
-		&pluginConfigFile,
+	runCmd.Flags().StringP(
 		"plugin-config", "p", "./gatewayd_plugins.yaml",
 		"plugin config file (default is ./gatewayd_plugins.yaml)")
 }
