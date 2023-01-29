7f726dceffad5fed3be96b08f16bca95d526a9c9
Mostafa Moradian <mostafa@gatewayd.io>
2023-01-29 22:15:06 +0100

    Add GatewayDError to handle errors and error wrapping
    Add error codes
    Add new errors

---
+++ errors/gatewayd_error.go
@@ -0,0 +1,35 @@
+package errors
+
+import "fmt"
+
+type ErrCode uint32
+
+type GatewayDError struct {
+	Code          ErrCode
+	Message       string
+	OriginalError error
+}
+
+func NewGatewaydError(code ErrCode, message string, err error) *GatewayDError {
+	return &GatewayDError{
+		Code:          code,
+		Message:       message,
+		OriginalError: err,
+	}
+}
+
+func (e *GatewayDError) Error() string {
+	if e.OriginalError == nil {
+		return e.Message
+	}
+	return fmt.Sprintf("%s, OriginalError: %s", e.Message, e.OriginalError)
+}
+
+func (e *GatewayDError) Wrap(err error) *GatewayDError {
+	e.OriginalError = err
+	return e
+}
+
+func (e *GatewayDError) Unwrap() error {
+	return e.OriginalError
+}
