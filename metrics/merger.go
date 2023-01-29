c63abd4fa1a2e418fc5f36da37c8a8c8e21f6770
Mostafa Moradian <mostafa@gatewayd.io>
2023-01-29 22:23:08 +0100

    Refactor ReadMetrics and Start to make them more readable and decouple them
    Add docstrings

--- metrics/merger.go
+++ metrics/merger.go
@@ -21,7 +21,7 @@ import (
 
 type IMerger interface {
 	Add(pluginName string, unixDomainSocket string)
-	ReadMetrics() (map[string]io.ReadCloser, error)
+	ReadMetrics() (map[string][]byte, error)
 	Start()
 	Stop()
 }
@@ -37,6 +37,7 @@ type Merger struct {
 
 var _ IMerger = &Merger{}
 
+// NewMerger creates a new metrics merger.
 func NewMerger(metricsMergerPeriod time.Duration, logger zerolog.Logger) *Merger {
 	return &Merger{
 		metricsMergerScheduler: gocron.NewScheduler(time.UTC),
@@ -47,6 +48,7 @@ func NewMerger(metricsMergerPeriod time.Duration, logger zerolog.Logger) *Merger
 	}
 }
 
+// Add adds a plugin and its unix domain socket to the map of plugins to merge metrics from.
 func (m *Merger) Add(pluginName string, unixDomainSocket string) {
 	if _, ok := m.Addresses[pluginName]; ok {
 		m.Logger.Warn().Fields(
@@ -59,8 +61,11 @@ func (m *Merger) Add(pluginName string, unixDomainSocket string) {
 	m.Addresses[pluginName] = unixDomainSocket
 }
 
-func (m *Merger) ReadMetrics() (map[string]io.ReadCloser, error) {
-	readers := make(map[string]io.ReadCloser)
+// ReadMetrics reads metrics from plugins by reading from their unix domain sockets.
+//
+//nolint:wrapcheck
+func (m *Merger) ReadMetrics() (map[string][]byte, error) {
+	readers := make(map[string][]byte)
 
 	for pluginName, unixDomainSocket := range m.Addresses {
 		if file, err := os.Stat(unixDomainSocket); err != nil || file.IsDir() || file.Mode().Type() != os.ModeSocket {
@@ -79,16 +84,30 @@ func (m *Merger) ReadMetrics() (map[string]io.ReadCloser, error) {
 		}
 
 		client := NewHTTPClientOverUDS(unixDomainSocket)
-		response, err := client.Get("http://plugins/metrics")
+		request, err := http.NewRequestWithContext(
+			context.Background(), http.MethodGet, "http://plugins/metrics", nil)
 		if err != nil {
 			return nil, err
 		}
-		readers[pluginName] = response.Body
+
+		response, err := client.Do(request)
+		if err != nil {
+			return nil, err
+		}
+		defer response.Body.Close()
+
+		metrics, err := io.ReadAll(response.Body)
+		if err != nil {
+			return nil, err
+		}
+
+		readers[pluginName] = metrics
 	}
 
 	return readers, nil
 }
 
+// Start starts the metrics merger.
 func (m *Merger) Start() {
 	// Merge metrics from plugins by reading from their unix domain sockets.
 	// This is done periodically.
@@ -98,54 +117,57 @@ func (m *Merger) Start() {
 		SingletonMode().
 		StartAt(time.Now().Add(m.MetricsMergerPeriod)).
 		Do(func() {
-			readers, err := m.ReadMetrics()
+			pluginMetrics, err := m.ReadMetrics()
 			if err != nil {
 				m.Logger.Error().Err(err).Msg("Failed to read plugin metrics")
+				return
 			}
 
 			// TODO: There should be a better, more efficient way to merge metrics from plugins.
 			var metricsOutput bytes.Buffer
 			enc := expfmt.NewEncoder(io.Writer(&metricsOutput), expfmt.FmtText)
-			for plugin, reader := range readers {
-				if reader != nil {
-					defer reader.Close()
+			for pluginName, metrics := range pluginMetrics {
+				if metrics == nil {
+					m.Logger.Trace().Str("plugin", pluginName).Msg("Plugin metrics are empty")
+					continue
+				}
 
-					// Retrieve plugin metrics.
-					textParser := expfmt.TextParser{}
-					if metrics, err := textParser.TextToMetricFamilies(reader); err != nil {
-						m.Logger.Error().Err(err).Msg("Failed to parse plugin metrics")
-					} else {
-						metricFamilies := map[string]*promClient.MetricFamily{}
+				// Retrieve plugin metrics.
+				textParser := expfmt.TextParser{}
+				reader := bytes.NewReader(metrics)
+				metrics, err := textParser.TextToMetricFamilies(reader)
+				if err != nil {
+					m.Logger.Error().Err(err).Msg("Failed to parse plugin metrics")
+					continue
+				}
 
-						for _, metric := range metrics {
-							for _, sample := range metric.Metric {
-								// Add plugin label to each metric.
-								sample.Label = append(sample.Label, &promClient.LabelPair{
-									Name:  proto.String("plugin"),
-									Value: proto.String(strings.ReplaceAll(plugin, "-", "_")),
-								})
-							}
-							metricFamilies[metric.GetName()] = metric
-						}
+				metricFamilies := map[string]*promClient.MetricFamily{}
+				for _, metric := range metrics {
+					for _, sample := range metric.Metric {
+						// Add plugin label to each metric.
+						sample.Label = append(sample.Label, &promClient.LabelPair{
+							Name:  proto.String("plugin"),
+							Value: proto.String(strings.ReplaceAll(pluginName, "-", "_")),
+						})
+					}
+					metricFamilies[metric.GetName()] = metric
+				}
 
-						metricNames := maps.Keys(metricFamilies)
-						sort.Strings(metricNames)
-
-						for _, metric := range metricNames {
-							err := enc.Encode(metricFamilies[metric])
-							if err != nil {
-								m.Logger.Error().Err(err).Msg("Failed to encode plugin metrics")
-								return
-							}
-						}
-
-						m.Logger.Debug().Fields(
-							map[string]interface{}{
-								"plugin": plugin,
-								"count":  len(metricNames),
-							}).Msgf("Processed and merged metrics")
+				metricNames := maps.Keys(metricFamilies)
+				sort.Strings(metricNames)
+				for _, metric := range metricNames {
+					err := enc.Encode(metricFamilies[metric])
+					if err != nil {
+						m.Logger.Error().Err(err).Msg("Failed to encode plugin metrics")
+						return
 					}
 				}
+
+				m.Logger.Debug().Fields(
+					map[string]interface{}{
+						"plugin": pluginName,
+						"count":  len(metricNames),
+					}).Msgf("Processed and merged metrics")
 			}
 
 			m.OutputMetrics = metricsOutput.Bytes()
@@ -156,6 +178,7 @@ func (m *Merger) Start() {
 	m.metricsMergerScheduler.StartAsync()
 }
 
+// Stop stops the metrics merger.
 func (m *Merger) Stop() {
 	m.metricsMergerScheduler.Clear()
 }
