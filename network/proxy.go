8db6338a54c7dcc6178a59fee40b24af5578d05d
Mostafa Moradian <mostafa@gatewayd.io>
2023-01-29 22:05:17 +0100

    Add proxy with elastic pool

--- network/proxy.go
+++ network/proxy.go
@@ -15,24 +15,30 @@ type Proxy interface {
 }
 
 type ProxyImpl struct {
-	pool             Pool
-	connectedClients map[gnet.Conn]Client
-	PoolSize         int
+	pool                Pool
+	connectedClients    map[gnet.Conn]Client
+	PoolSize            int
+	Elastic             bool
+	ReuseElasticClients bool
 }
 
 var _ Proxy = &ProxyImpl{}
 
-func NewProxy(size int) *ProxyImpl {
+func NewProxy(size int, elastic, reuseElasticClients bool) *ProxyImpl {
 	proxy := ProxyImpl{
-		pool:             NewPool(),
-		connectedClients: make(map[gnet.Conn]Client),
-		PoolSize:         size,
+		pool:                NewPool(),
+		connectedClients:    make(map[gnet.Conn]Client),
+		PoolSize:            size,
+		Elastic:             elastic,
+		ReuseElasticClients: reuseElasticClients,
 	}
 
-	for i := 0; i < size; i++ {
-		c := NewClient("tcp", "localhost:5432", 4096)
-		if err := proxy.pool.Put(c); err != nil {
-			logrus.Panic(err)
+	if !proxy.Elastic {
+		for i := 0; i < size; i++ {
+			c := NewClient("tcp", "localhost:5432", 4096)
+			if err := proxy.pool.Put(c); err != nil {
+				logrus.Panic(err)
+			}
 		}
 	}
 
@@ -43,10 +49,18 @@ func (pr *ProxyImpl) Connect(c gnet.Conn) error {
 	clientIDs := pr.pool.GetClientIDs()
 	if len(clientIDs) == 0 {
 		logrus.Error("No clients available")
+	}
+
+	var client Client
+	if len(clientIDs) == 0 && pr.Elastic {
+		client = NewClient("tcp", "localhost:5432", 4096)
+		logrus.Infof("Elastic client connection %s has been created", client.ID)
+	} else if len(clientIDs) > 0 {
+		client = pr.pool.Pop(clientIDs[0])
+	} else {
 		return errors.New("no clients available")
 	}
 
-	client := pr.pool.Pop(clientIDs[0])
 	logrus.Infof("Client %s has been assigned to %s", client.ID, c.RemoteAddr().String())
 	pr.connectedClients[c] = client
 
@@ -55,7 +69,15 @@ func (pr *ProxyImpl) Connect(c gnet.Conn) error {
 
 func (pr *ProxyImpl) Disconnect(c gnet.Conn) error {
 	client := pr.connectedClients[c]
-	pr.pool.Put(client)
+	if pr.Elastic && pr.ReuseElasticClients {
+		pr.pool.Put(client)
+		logrus.Infof("Elastic client connection %s has been put on the pool for reuse", client.ID)
+	} else if !pr.Elastic {
+		pr.pool.Put(client)
+	} else {
+		client.Close()
+		logrus.Infof("Elastic client connection %s has been closed", client.ID)
+	}
 	delete(pr.connectedClients, c)
 
 	return nil
