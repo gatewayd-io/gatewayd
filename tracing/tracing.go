0495f5955feccbf4648432adf5b9a92ea98eaaed
Mostafa Moradian <mostafa@gatewayd.io>
2023-02-19 00:11:58 +0100

    Add tracing package and OTLPTracer initializer

---
+++ tracing/tracing.go
@@ -0,0 +1,61 @@
+package tracing
+
+import (
+	"context"
+	"log"
+
+	"github.com/gatewayd-io/gatewayd/config"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"google.golang.org/grpc/credentials"
+
+	"go.opentelemetry.io/otel/sdk/resource"
+	"go.opentelemetry.io/otel/sdk/trace"
+)
+
+func OTLPTracer(insecure bool, collectorURL, serviceName string) func(context.Context) error {
+	secureOption := otlptracegrpc.WithTLSCredentials(credentials.NewClientTLSFromCert(nil, ""))
+	if insecure {
+		secureOption = otlptracegrpc.WithInsecure()
+	}
+
+	exporter, err := otlptrace.New(
+		context.Background(),
+		otlptracegrpc.NewClient(
+			secureOption,
+			otlptracegrpc.WithEndpoint(collectorURL),
+		),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	resources, err := resource.New(
+		context.Background(),
+		resource.WithAttributes(
+			attribute.String("service.name", serviceName),
+			attribute.String("service.version", config.Version),
+			attribute.String("library.language", "go"),
+		),
+	)
+	if err != nil {
+		// logger.Error().Err(err).Msg("Could not set resources")
+		log.Fatal(err)
+	}
+
+	resources, _ = resource.Merge(
+		resource.Default(),
+		resources,
+	)
+
+	otel.SetTracerProvider(
+		trace.NewTracerProvider(
+			trace.WithSampler(trace.AlwaysSample()),
+			trace.WithBatcher(exporter),
+			trace.WithResource(resources),
+		),
+	)
+	return exporter.Shutdown
+}
