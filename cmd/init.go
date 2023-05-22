3ec8f17d324d44bd691355f2adc1da680db85f93
Mostafa Moradian <mostafa@gatewayd.io>
2023-05-22 22:49:03 +0200

    Remove data/time from logger

--- cmd/init.go
+++ cmd/init.go
@@ -21,6 +21,8 @@ var initCmd = &cobra.Command{
 	Use:   "init",
 	Short: "Create or overwrite the GatewayD global config",
 	Run: func(cmd *cobra.Command, args []string) {
+		logger := log.New(cmd.OutOrStdout(), "", 0)
+
 		// Create a new config object and load the defaults.
 		conf := &config.Config{
 			GlobalKoanf: koanf.New("."),
@@ -30,27 +32,27 @@ var initCmd = &cobra.Command{
 		// Marshal the global config to YAML.
 		globalCfg, err := conf.GlobalKoanf.Marshal(yaml.Parser())
 		if err != nil {
-			log.Fatal(err)
+			logger.Fatal(err)
 		}
 
 		// Check if the config file already exists and if we should overwrite it.
 		exists := false
 		if _, err := os.Stat(globalConfigFile); err == nil && !force {
-			log.Fatal("Config file already exists. Use --force to overwrite.")
+			logger.Fatal("Config file already exists. Use --force to overwrite.")
 		} else if err == nil {
 			exists = true
 		}
 
 		// Create or overwrite the global config file.
 		if err := os.WriteFile(globalConfigFile, globalCfg, filePermissions); err != nil {
-			log.Fatal(err)
+			logger.Fatal(err)
 		}
 
 		verb := "created"
 		if exists && force {
 			verb = "overwritten"
 		}
-		log.Printf("Config file '%s' was %s successfully.", globalConfigFile, verb)
+		logger.Printf("Config file '%s' was %s successfully.", globalConfigFile, verb)
 	},
 }
 
