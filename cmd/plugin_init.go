3d93d48503884799dd187d751a4497f6b36d15d4
Mostafa Moradian <mostafa@gatewayd.io>
2023-06-04 00:59:28 +0200

    Generate plugin config

---
+++ cmd/plugin_init.go
@@ -0,0 +1,26 @@
+package cmd
+
+import (
+	"github.com/gatewayd-io/gatewayd/config"
+	"github.com/spf13/cobra"
+)
+
+// pluginInitCmd represents the plugin init command.
+var pluginInitCmd = &cobra.Command{
+	Use:   "init",
+	Short: "Create or overwrite the GatewayD plugins config",
+	Run: func(cmd *cobra.Command, args []string) {
+		generateConfig(cmd, Plugin, pluginConfigFile, force)
+	},
+}
+
+func init() {
+	pluginCmd.AddCommand(pluginInitCmd)
+
+	pluginInitCmd.Flags().BoolVarP(
+		&force, "force", "f", false, "Force overwrite of existing config file")
+	pluginInitCmd.Flags().StringVarP(
+		&pluginConfigFile, // Already exists in run.go
+		"plugin-config", "p", config.GetDefaultConfigFilePath(config.PluginsConfigFilename),
+		"Plugin config file")
+}
