4e52b24cea4246ec180bfa1f005afda9ecd1f455
Mostafa Moradian <mostafa@gatewayd.io>
2023-06-06 00:32:58 +0200

    Fix wording

--- cmd/config.go
+++ cmd/config.go
@@ -9,7 +9,7 @@ import (
 // configCmd represents the config command.
 var configCmd = &cobra.Command{
 	Use:   "config",
-	Short: "Manage GatewayD configuration",
+	Short: "Manage GatewayD global configuration",
 	Run: func(cmd *cobra.Command, args []string) {
 		if err := cmd.Help(); err != nil {
 			log.New(cmd.OutOrStdout(), "", 0).Fatal(err)
