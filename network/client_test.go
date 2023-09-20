b84cae6d2f016eb7fbd417610d54366741a8f933
Mostafa Moradian <mostafa@gatewayd.io>
2023-09-20 22:44:25 +0200

    Add benchmark for network/client

--- network/client_test.go
+++ network/client_test.go
@@ -106,3 +106,115 @@ func TestIsConnected(t *testing.T) {
 	client.Close()
 	assert.False(t, client.IsConnected())
 }
+
+func BenchmarkNewClient(b *testing.B) {
+	cfg := logging.LoggerConfig{
+		Output:            []config.LogOutput{config.Console},
+		TimeFormat:        zerolog.TimeFormatUnix,
+		ConsoleTimeFormat: time.RFC3339,
+		Level:             zerolog.DebugLevel,
+		NoColor:           true,
+	}
+
+	logger := logging.NewLogger(context.Background(), cfg)
+	for i := 0; i < b.N; i++ {
+		c := NewClient(context.Background(), &config.Client{
+			Network:            "tcp",
+			Address:            "localhost:5432",
+			ReceiveChunkSize:   config.DefaultChunkSize,
+			ReceiveDeadline:    config.DefaultReceiveDeadline,
+			SendDeadline:       config.DefaultSendDeadline,
+			TCPKeepAlive:       false,
+			TCPKeepAlivePeriod: config.DefaultTCPKeepAlivePeriod,
+		}, logger)
+		c.Close()
+	}
+}
+
+func BenchmarkSend(b *testing.B) {
+	logger := logging.NewLogger(context.Background(), logging.LoggerConfig{
+		Output:            []config.LogOutput{config.Console},
+		TimeFormat:        zerolog.TimeFormatUnix,
+		ConsoleTimeFormat: time.RFC3339,
+		Level:             zerolog.DebugLevel,
+		NoColor:           true,
+	})
+
+	client := NewClient(
+		context.Background(),
+		&config.Client{
+			Network:            "tcp",
+			Address:            "localhost:5432",
+			ReceiveChunkSize:   config.DefaultChunkSize,
+			ReceiveDeadline:    config.DefaultReceiveDeadline,
+			SendDeadline:       config.DefaultSendDeadline,
+			TCPKeepAlive:       false,
+			TCPKeepAlivePeriod: config.DefaultTCPKeepAlivePeriod,
+		},
+		logger)
+	defer client.Close()
+
+	packet := CreatePgStartupPacket()
+	for i := 0; i < b.N; i++ {
+		client.Send(packet)
+	}
+}
+
+func BenchmarkReceive(b *testing.B) {
+	logger := logging.NewLogger(context.Background(), logging.LoggerConfig{
+		Output:            []config.LogOutput{config.Console},
+		TimeFormat:        zerolog.TimeFormatUnix,
+		ConsoleTimeFormat: time.RFC3339,
+		Level:             zerolog.DebugLevel,
+		NoColor:           true,
+	})
+
+	client := NewClient(
+		context.Background(),
+		&config.Client{
+			Network:            "tcp",
+			Address:            "localhost:5432",
+			ReceiveChunkSize:   config.DefaultChunkSize,
+			ReceiveDeadline:    config.DefaultReceiveDeadline,
+			ReceiveTimeout:     1 * time.Millisecond,
+			SendDeadline:       config.DefaultSendDeadline,
+			TCPKeepAlive:       false,
+			TCPKeepAlivePeriod: config.DefaultTCPKeepAlivePeriod,
+		},
+		logger)
+	defer client.Close()
+
+	packet := CreatePgStartupPacket()
+	client.Send(packet)
+	for i := 0; i < b.N; i++ {
+		client.Receive()
+	}
+}
+
+func BenchmarkIsConnected(b *testing.B) {
+	logger := logging.NewLogger(context.Background(), logging.LoggerConfig{
+		Output:            []config.LogOutput{config.Console},
+		TimeFormat:        zerolog.TimeFormatUnix,
+		ConsoleTimeFormat: time.RFC3339,
+		Level:             zerolog.DebugLevel,
+		NoColor:           true,
+	})
+
+	client := NewClient(
+		context.Background(),
+		&config.Client{
+			Network:            "tcp",
+			Address:            "localhost:5432",
+			ReceiveChunkSize:   config.DefaultChunkSize,
+			ReceiveDeadline:    config.DefaultReceiveDeadline,
+			SendDeadline:       config.DefaultSendDeadline,
+			TCPKeepAlive:       false,
+			TCPKeepAlivePeriod: config.DefaultTCPKeepAlivePeriod,
+		},
+		logger)
+	defer client.Close()
+
+	for i := 0; i < b.N; i++ {
+		client.IsConnected()
+	}
+}
