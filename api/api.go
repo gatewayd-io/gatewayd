18477ec9d1cda560dab2ce4487574505acafc063
saba <smnmna99@gmail.com>
2024-04-27 14:55:27 +0200

    sign commit

--- api/api.go
+++ api/api.go
@@ -5,6 +5,7 @@ package api
 import (
 	"context"
 	"encoding/json"
+
 	sdkPlugin "github.com/gatewayd-io/gatewayd-plugin-sdk/plugin"
 	v1 "github.com/gatewayd-io/gatewayd/api/v1"
 	"github.com/gatewayd-io/gatewayd/config"
