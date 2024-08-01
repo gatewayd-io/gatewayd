71433828ca590812c49c7fc3dc51f2954f5e4aae
sinadarbouy <sinadarbouy@gmail.com>
2024-08-01 21:57:51 +0200

    convert all refrereced configblock to lowercase

--- config/constants.go
+++ config/constants.go
@@ -35,7 +35,7 @@ const (
 const (
 	// Config constants.
 	Default                   = "default"
-	DefaultConfigurationBlock = "activeWrites"
+	DefaultConfigurationBlock = "active-writes"
 	EnvPrefix                 = "GATEWAYD_"
 	TracerName                = "gatewayd"
 	GlobalConfigFilename      = "gatewayd.yaml"
