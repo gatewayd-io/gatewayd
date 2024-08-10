6e219f313c277025cd67a721be8a364ce100c0ee
sinadarbouy <sinadarbouy@gmail.com>
2024-08-10 17:33:04 +0200

    Improve WeightedRoundRobin Algorithem

--- network/weightedroundrobin.go
+++ network/weightedroundrobin.go
@@ -2,44 +2,74 @@ package network
 
 import (
 	"errors"
-	"sync/atomic"
 
 	"github.com/gatewayd-io/gatewayd/config"
 	gerr "github.com/gatewayd-io/gatewayd/errors"
 )
 
+type weightedProxy struct {
+	proxy           IProxy
+	weight          int
+	currentWeight   int
+	effectiveWeight int
+}
+
 type WeightedRoundRobin struct {
-	proxies []IProxy
-	next    atomic.Uint32
+	proxies     []*weightedProxy
+	totalWeight int
 }
 
 func NewWeightedRoundRobin(server *Server, loadbalancerRule config.LoadBalancingRule) *WeightedRoundRobin {
-	var proxies []IProxy
+	var proxies []*weightedProxy
+	totalWeight := 0
 
-	// Populate the cyclic list of proxies according to their weights.
 	for _, distribution := range loadbalancerRule.Distribution {
 		proxy := findProxyByName(distribution.ProxyName, server.Proxies)
 		if proxy != nil {
-			for i := 0; i < distribution.Weight; i++ {
-				proxies = append(proxies, proxy)
-			}
+			weight := distribution.Weight
+			totalWeight += weight
+			proxies = append(proxies, &weightedProxy{
+				proxy:           proxy,
+				weight:          weight,
+				effectiveWeight: weight,
+				currentWeight:   0,
+			})
 		}
 	}
 
 	return &WeightedRoundRobin{
-		proxies: proxies,
+		proxies:     proxies,
+		totalWeight: totalWeight,
 	}
 }
 
+// NextProxy selects the next proxy based on the weighted round-robin algorithm.
 func (r *WeightedRoundRobin) NextProxy() (IProxy, *gerr.GatewayDError) {
-	proxiesLen := uint32(len(r.proxies))
-	if proxiesLen == 0 {
+
+	if len(r.proxies) == 0 {
 		return nil, gerr.ErrNoProxiesAvailable.Wrap(errors.New("proxy list is empty"))
 	}
-	nextIndex := r.next.Add(1)
-	return r.proxies[nextIndex%proxiesLen], nil
+
+	var selected *weightedProxy
+
+	// Adjust weights and select the proxy with the highest current weight.
+	for _, p := range r.proxies {
+		p.currentWeight += p.effectiveWeight
+		if selected == nil || p.currentWeight > selected.currentWeight {
+			selected = p
+		}
+	}
+
+	// Reduce the selected proxy's current weight by the total weight.
+	if selected != nil {
+		selected.currentWeight -= r.totalWeight
+		return selected.proxy, nil
+	}
+
+	return nil, gerr.ErrNoProxiesAvailable.Wrap(errors.New("no proxy selected"))
 }
 
+// findProxyByName finds a proxy by name from the list of proxies.
 func findProxyByName(name string, proxies []IProxy) IProxy {
 	for _, proxy := range proxies {
 		if proxy.GetName() == name {
