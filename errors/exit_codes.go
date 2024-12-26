2a005f81a4527b6e713460763283180e34cfcfeb
Mostafa Moradian <mostafa@gatewayd.io>
2024-12-26 14:10:51 +0100

    Remove unused constant and renumber others

--- errors/exit_codes.go
+++ errors/exit_codes.go
@@ -1,12 +1,11 @@
 package errors
 
 const (
-	FailedToCreateClient         = 1
-	FailedToStartServer          = 2
-	FailedToStartTracer          = 3
-	FailedToCreateActRegistry    = 4
-	FailedToLoadPolicies         = 5
-	FailedToStartRaftNode        = 6
-	FailedToMergeGlobalConfig    = 7
-	FailedToCreatePoolAndClients = 8
+	FailedToStartServer          = 1
+	FailedToStartTracer          = 2
+	FailedToCreateActRegistry    = 3
+	FailedToLoadPolicies         = 4
+	FailedToStartRaftNode        = 5
+	FailedToMergeGlobalConfig    = 6
+	FailedToCreatePoolAndClients = 7
 )
